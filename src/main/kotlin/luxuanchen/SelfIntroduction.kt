f0c092b6918544de60df161b00803c19499d88d5
niaguiii <luxuanchen499@gmail.com>
2025-03-13 23:05:17 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/luxuanchen/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.luxuanchen
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Kenny Cheng.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
